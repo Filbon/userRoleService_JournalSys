803dd9937ad16098fec9ee72175d28cc27fa9d9d
Labb3A <filipkleimert@gmail.com>
2024-12-07 14:27:12 +0100

    Docker

--- src/main/java/com/example/patientservice_journalsys/Controller/userRoleController.java
+++ src/main/java/com/example/patientservice_journalsys/Controller/userRoleController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://kubernetes.docker.internal:30000")
 @RestController
 @RequestMapping("/api/userRole")
 public class userRoleController {
